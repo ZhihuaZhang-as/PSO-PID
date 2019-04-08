bd282da24e1a00da187182049364185976561069
TianZhendong <33273211+TianZhendong@users.noreply.github.com>
2019-04-08 16:01:02 +0800

    fitness

--- PSO-PID.m
+++ PSO-PID.m
@@ -3,4 +3,4 @@ assignin('base','Kp',x(1));
 assignin('base','Ki',x(2));
 assignin('base','Kd',x(3));
 [~,~,y_out]=sim('thetapid',[0,50]);
-z=y_out(end,1);   %%表示 取这个 矩阵的第一列最后一行的数据。
+z=y_out(end,1);   %%表示 取这个矩阵的第一列最后一行的数据。

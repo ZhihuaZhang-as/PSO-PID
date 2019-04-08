7d8eb559ed4623cfbf8de662c4539014feb7a9aa
TianZhendong <33273211+TianZhendong@users.noreply.github.com>
2019-04-08 15:42:59 +0800

    Create PSO-PID.m

---
+++ PSO-PID.m
@@ -0,0 +1,6 @@
+function z=PSO_PID(x)
+assignin('base','Kp',x(1));
+assignin('base','Ki',x(2));
+assignin('base','Kd',x(3));
+[~,~,y_out]=sim('thetapid',[0,50]);
+z=y_out(end,1);   %%表示 取这个 矩阵的第一列最后一行的数据。
